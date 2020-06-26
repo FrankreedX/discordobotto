624079179ca9a55e372aa4c1019d814974a36f6a
Frankreed <baoanhtdn@gmail.com>
2020-06-25 22:09:00 -0700

    added database to manage collections, database not working tho. calling database function from main won't return desired string

---
+++ src/main/kotlin/Database.kt
@@ -0,0 +1,21 @@
+import kotlin.random.Random
+
+class Database {
+    private val roasts = listOf(
+        "I know 5 fat people in this server and you're 3 of them",
+        "You egg?",
+        "ur mom",
+        "you walking in front of an air purifier makes it work full power"
+    )
+    private val hentai = listOf(
+        "やめて!",
+        "だめ！",
+        "おちんちんがつごいです"
+    )
+    fun getRandomRoasts():String{
+        return roasts[Random.nextInt()]
+    }
+    fun getRandomHentai():String{
+        return hentai[Random.nextInt()]
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,25 @@
 import com.gitlab.kordlib.core.Kord
 import com.gitlab.kordlib.core.event.message.MessageCreateEvent
 import com.gitlab.kordlib.core.on
-import kotlin.random.Random
 
 suspend fun main(){
     val bot = Kord("NzI1ODUwNTcyOTM4MjgwOTgx.XvVeDQ._tkA1qN7-hNaZ1sIpNaLGpByKpU")
-    val hentiWordPool = listOf("やめて!", "だめ！","おちんちんがつごいです")
+    val database = Database()
     bot.on<MessageCreateEvent> {
-        if(message.author!!.isBot==null) {
+        println(database.getRandomHentai())
+        if(message.author!!.isBot==null){
             if (message.content == "!shove" || message.content == "!push") {
-                message.channel.createMessage(hentiWordPool[Random.nextInt(0, 3)])
+                println(" asdfwae")
+                println(database.getRandomHentai())
+                message.channel.createMessage(database.getRandomHentai())
+            }
+            if (message.content == "!roastme"){
+                message.channel.createMessage("say pls bitch")
+            }
+            if (message.content == "!plsroastme"){
+                message.channel.createMessage(database.getRandomRoasts())
+                println(" itasf")
+                println(database.getRandomRoasts())
             }
         }
     }
